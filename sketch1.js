60b5a7f668b4285abd9106a30e21532c0c3e62c4
zeelac <luca.zeller@hotmail.com>
2017-01-20 23:42:25 +0100

    Update sketch1.js

--- sketch1.js
+++ sketch1.js
@@ -1,7 +1,7 @@
 var sliderB, sliderH;
 
 function setup () {
-  createCanvas(displayWidth-200, displayHeight-200);
+  createCanvas(displayWidth-500, displayHeight-500);
   background(123);
   sliderB = createSlider(0, 200, 50);
   sliderH = createSlider(0, 200, 50);
