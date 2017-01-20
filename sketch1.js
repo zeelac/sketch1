8c62acf6cfe6b344f55282a684bea8518775060f
zeelac <luca.zeller@hotmail.com>
2017-01-20 23:47:41 +0100

    Update sketch1.js

--- sketch1.js
+++ sketch1.js
@@ -2,14 +2,15 @@ var sliderB, sliderH;
 
 function setup () {
   createCanvas(displayWidth-500, displayHeight-500);
-  background(123);
+  //background(123);
   sliderB = createSlider(0, 200, 50);
   sliderH = createSlider(0, 200, 50);
 }
 
 function draw() {
+  background(random(100,150);
   fill(random(255), 0, 50);
   noStroke();
-  rect(touchX, touchY, sliderB.value(), sliderH.value());
+  ellipse(touchX, touchY, sliderB.value(), sliderH.value());
   console.log('sciiiddiby doo');
 }
