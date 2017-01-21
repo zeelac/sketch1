9353cb94f3244a9f7c7d96fc5d6fc89dfae214fc
zeelac <luca.zeller@hotmail.com>
2017-01-21 01:31:46 +0100

    Update sketch1.js

--- sketch1.js
+++ sketch1.js
@@ -16,6 +16,13 @@ function draw() {
   ellipse(mouseX, mouseY, sliderB.value(), sliderH.value());
   fill(random(255), 0, 50);
   noStroke();
+  
+  if (accelerationX > 2) {
+   scale += 0.1; 
+   if (scale > 1) {
+    scale = 0.1;
+   }
+  }
 
   image(capture, touchX, touchY, 320*scale, 240*scale);
 }
